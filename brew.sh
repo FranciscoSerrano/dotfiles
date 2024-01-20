04d7e6c37a066546651abf77d4141072bcdc9415
Francisco Serrano <fserrano@apple.com>
2024-01-20 10:51:48 -0800

    removed unsed configs

--- brew.sh
+++ brew.sh
@@ -11,44 +11,13 @@ brew tap caskroom/cask
 
 echo "Installing your perferred tools and apps..."
 # Programming Languages
-brew install node
-brew install python
-brew cask install java
-brew cask install anaconda
+brew install deno rust lua
 
 # Dev Tools
-brew install git
-brew install htop
-brew install tree
-brew install docker
-brew install neofetch
-brew install starship #shell theme
-brew cask install balenaetcher
-brew cask install visual-studio-code
+brew install git tree starship neofetch tmux helix keka
 
 # Communication Apps
-brew cask install slack
-brew cask install discord
+brew cask install slack discord
 
 # Web Browsers
-brew cask install firefox
-brew cask install tor-browser
-brew cask install brave-browser
-
-#File Encryption
-brew cask install keka
-brew cask install 1password
-brew install jesseduffield/horcrux/horcrux
-
-# Writing Apps
-brew install vim
-
-# Entertainment
-brew install cowsay
-brew install cmatrix
-brew cask install obs
-brew cask install vlc
-brew cask install steam
-brew cask install calibre
-brew cask install minecraft
-brew cask install transmission
+brew cask install firefox tor-browser
