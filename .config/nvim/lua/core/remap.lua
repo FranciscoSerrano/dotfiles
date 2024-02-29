9cc1157c172367e39110eb44fb58ce64a33b5127
Francisco Serrano <fserrano@apple.com>
2024-02-29 10:36:47 -0800

    added comments for clairity

--- .config/nvim/lua/core/remap.lua
+++ .config/nvim/lua/core/remap.lua
@@ -1,56 +1,56 @@
-
+-- sets the leader key to the space key
 vim.g.mapleader = " "
+-- opens explore. Mnemonic: Project View
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- When text is selected, you can move the highlighted text around the page.
+-- very useful
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- appends the line below it to the end while keeping cursor in place
 vim.keymap.set("n", "J", "mzJ`z")
+-- moves down or up half a page while keeping cursor centered
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- keeps search terms in the middle when cycling up or down
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
-
 -- greatest remap ever
+-- will paste a word over a highlighted word while preserving your copied word
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
+-- yank to system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
+-- this is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+-- never press Q. 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
+-- not sure what these do yet
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+-- replace whatever word you are currently on
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- make a file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set(
-    "n",
-    "<leader>ee",
-    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
-)
-
+-- just for fun
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
+-- source current file
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
-
