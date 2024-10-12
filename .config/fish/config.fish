d23e29833fb449436a27ddf95452f3de27bb5ea7
Francisco Serrano <fserrano@apple.com>
2024-10-11 20:01:07 -0700

    Fish config cleanup and added vim key bindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -74,39 +74,6 @@ function ls
 	lsd $argv
 end
 
-function openRadarNumber
-	if test $argv[1]
-		echo "Opening $argv[1] in Radar..."
-		open "rdar://$argv[1]"
-	else
-		echo "No radar number given, attempting to find radar from GIT branch."
-
-		openRadarSCM
-	end
-end
-
-function submit
-    # Check if all three arguments are provided
-    if test (count $argv) -ne 3
-        echo "Usage: submit tag gitURL train"
-		echo "	- parameter tag: The git tag you wish to submit to the build"
-		echo "	- parameter gitURL: The git url for the project the tag belongs to"
-		echo "	- parameter train: The train you wish to submit to."
-        return 1
-    end
-
-    set tag $argv[1]
-    set gitURL $argv[2]
-    set train $argv[3]
-
-	submitproject -noLogging -git -url $gitURL -tag $tag $train
-    return
-end
-
-function openCurrentXcode
-	open (xcode-select -p | grep -io ".*Xcode\.app")
-end
-
 function ox
 	open *.xcodeproj
 end
@@ -119,20 +86,8 @@ function lsp
 	log show --predicate $argv
 end
 
-function knox-download-macOS-build
-	nsd --knox --type assistant-image --image-name "UniversalMacInternalAssistant" --buildversion $argv --output-descriptive-name
-end
-
 function whatsa
 	device_model_map -like $argv[1]
-end
-
-function setupautomation
-	cd ~/Projects/SetupAutomation && hx ./
-end
-
-function notes
-	cd ~/Documents/MarkdownNotes && hx ./
 end
 
 function updateMacOS
@@ -161,3 +116,12 @@ starship init fish | source
 
 # --- Path Modifications ---
 set -x PATH "/Users/franciscoserrano/.dotfiles/bin/" "$PATH"
+
+# --- Vim Key Bindings ---
+
+fish_vi_key_bindings
+
+set -Ux EDITOR nvim
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
